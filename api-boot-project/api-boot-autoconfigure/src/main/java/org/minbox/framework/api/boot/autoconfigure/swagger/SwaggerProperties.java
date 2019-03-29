d913b447df00c858f9692bbde94a5b135aa80c06
恒宇少年 <jnyuqy@dingtalk.com>
2019-03-29 16:44:53 +0800

    发布1.0.1.RELEASE版本

--- api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
+++ api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
@@ -45,7 +45,7 @@ public class SwaggerProperties {
     /**
      * 文档版本号
      */
-    private String version = "1.0.2.RC1";
+    private String version = "1.0.1.RELEASE";
     /**
      * 文档版权
      */
