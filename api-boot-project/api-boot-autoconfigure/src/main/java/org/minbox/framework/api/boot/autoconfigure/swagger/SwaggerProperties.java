401e6dc74e8221ed337b97612bbf87b4aaea2625
恒宇少年 <jnyuqy@dingtalk.com>
2019-04-04 14:50:49 +0800

    ApiBoot DataSource Switch 自动化配置添加

--- api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
+++ api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
@@ -45,7 +45,7 @@ public class SwaggerProperties {
     /**
      * 文档版本号
      */
-    private String version = "1.0.2.RELEASE";
+    private String version = "1.0.3.RELEASE";
     /**
      * 文档版权
      */
