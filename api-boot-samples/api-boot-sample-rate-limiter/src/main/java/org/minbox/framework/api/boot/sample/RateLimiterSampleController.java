997adbcd0444d5e3172dfb4c4e5cda8325ac9f7d
恒宇少年 <jnyuqy@dingtalk.com>
2019-04-26 14:43:24 +0800

    接口限流示例

---
+++ api-boot-samples/api-boot-sample-rate-limiter/src/main/java/org/minbox/framework/api/boot/sample/RateLimiterSampleController.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright [2019] [恒宇少年 - 于起宇]
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package org.minbox.framework.api.boot.sample;
+
+import org.minbox.framework.api.boot.plugin.rate.limiter.annotation.RateLimiter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author：恒宇少年 - 于起宇
+ * <p>
+ * DateTime：2019-04-26 14:39
+ * Blog：http://blog.yuqiyu.com
+ * WebSite：http://www.jianshu.com/u/092df3f77bca
+ * Gitee：https://gitee.com/hengboy
+ * GitHub：https://github.com/hengboy
+ */
+@RestController
+@RequestMapping(value = "/test")
+public class RateLimiterSampleController {
+    /**
+     * QPS默认为1
+     *
+     * @return
+     */
+    @GetMapping(value = "/")
+    @RateLimiter(QPS = 10)
+    public String getContent() {
+        return "访问资源";
+    }
+}
