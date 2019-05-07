a2916d0ea65256786acfbd3022574c2aecf5efbd
恒宇少年 <jnyuqy@dingtalk.com>
2019-05-07 15:59:26 +0800

    ApiBoot RateLimiter 添加默认的Config Centre

---
+++ api-boot-project/api-boot-plugins/api-boot-plugin-rate-limiter/src/main/java/org/minbox/framework/api/boot/plugin/rate/limiter/centre/support/DefaultRateLimiterConfigCentre.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright [2019] [恒宇少年 - 于起宇]
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package org.minbox.framework.api.boot.plugin.rate.limiter.centre.support;
+
+import org.minbox.framework.api.boot.common.exception.ApiBootException;
+
+/**
+ * default rate limiter config centre
+ *
+ * @author：恒宇少年 - 于起宇
+ * <p>
+ * DateTime：2019-05-07 15:51
+ * Blog：http://blog.yuqiyu.com
+ * WebSite：http://www.jianshu.com/u/092df3f77bca
+ * Gitee：https://gitee.com/hengboy
+ * GitHub：https://github.com/hengboy
+ */
+public class DefaultRateLimiterConfigCentre extends AbstractRateLimiterConfigCentre {
+    @Override
+    public Long getQps(String configKey) throws ApiBootException {
+        return DEFAULT_QPS;
+    }
+
+    @Override
+    public void setQps(String configKey, Long QPS) throws ApiBootException {
+        //...
+    }
+}
