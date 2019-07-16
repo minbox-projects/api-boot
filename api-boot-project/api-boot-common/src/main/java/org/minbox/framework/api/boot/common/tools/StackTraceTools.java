1465e7d9f33949939ad4d5e4387941d9c638bafe
hengboy <jnyuqy@dingtalk.com>
2019-07-16 17:43:15 +0800

    添加获取异常堆栈信息工具类

---
+++ api-boot-project/api-boot-common/src/main/java/org/minbox/framework/api/boot/common/tools/StackTraceTools.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright [2019] [恒宇少年 - 于起宇]
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package org.minbox.framework.api.boot.common.tools;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Exception Stack Trace Tool
+ *
+ * @author：恒宇少年 - 于起宇
+ * <p>
+ * DateTime：2019-07-15 17:53
+ * Blog：http://blog.yuqiyu.com
+ * WebSite：http://www.jianshu.com/u/092df3f77bca
+ * Gitee：https://gitee.com/hengboy
+ * GitHub：https://github.com/hengboy
+ */
+public class StackTraceTools {
+
+    /**
+     * Get Exception Stack Trace
+     *
+     * @param throwable Exception Instace
+     * @return Stack Trace
+     */
+    public static String getStackTrace(Throwable throwable) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        try {
+            throwable.printStackTrace(pw);
+            return sw.toString();
+        } finally {
+            pw.close();
+        }
+    }
+}
