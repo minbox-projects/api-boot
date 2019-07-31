b0b8e4d63e504a94dca60180d062e00f670c1624
hengboy <jnyuqy@dingtalk.com>
2019-07-31 09:35:04 +0800

    独立拆分ApiBoot Logging 组件上报指定Admin地址的自动配置

---
+++ api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/logging/ApiBootLoggingAdminAppointAutoConfiguration.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright [2019] [恒宇少年 - 于起宇]
+ *
+ *      Licensed under the Apache License, Version 2.0 (the "License");
+ *      you may not use this file except in compliance with the License.
+ *      You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *      Unless required by applicable law or agreed to in writing, software
+ *      distributed under the License is distributed on an "AS IS" BASIS,
+ *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *      See the License for the specific language governing permissions and
+ *      limitations under the License.
+ *
+ */
+
+package org.minbox.framework.api.boot.autoconfigure.logging;
+
+import org.minbox.framework.api.boot.plugin.logging.admin.discovery.support.LoggingAppointAdminDiscovery;
+import org.minbox.framework.api.boot.plugin.logging.admin.discovery.support.LoggingRegistryCenterAdminDiscovery;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.minbox.framework.api.boot.autoconfigure.logging.ApiBootLoggingProperties.API_BOOT_LOGGING_PREFIX;
+
+/**
+ * ApiBoot Logging Admin Appoint Configuration
+ *
+ * @author：恒宇少年 - 于起宇
+ * <p>
+ * DateTime：2019-07-26 15:19
+ * Blog：http://blog.yuqiyu.com
+ * WebSite：http://www.jianshu.com/u/092df3f77bca
+ * Gitee：https://gitee.com/hengboy
+ * GitHub：https://github.com/hengboy
+ */
+@Configuration
+@EnableConfigurationProperties(ApiBootLoggingProperties.class)
+@ConditionalOnMissingBean(LoggingRegistryCenterAdminDiscovery.class)
+@ConditionalOnProperty(prefix = API_BOOT_LOGGING_PREFIX, name = "admin.server-address")
+public class ApiBootLoggingAdminAppointAutoConfiguration {
+    /**
+     * ApiBoot Logging Properties
+     */
+    private ApiBootLoggingProperties apiBootLoggingProperties;
+
+    public ApiBootLoggingAdminAppointAutoConfiguration(ApiBootLoggingProperties apiBootLoggingProperties) {
+        this.apiBootLoggingProperties = apiBootLoggingProperties;
+    }
+
+    /**
+     * ApiBoot Logging Admin Config Discovery
+     * Multiple Use "," Separation
+     *
+     * @return LoggingAdminDiscovery
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public LoggingAppointAdminDiscovery loggingConfigAdminDiscovery() {
+        String[] serverAddressArray = apiBootLoggingProperties.getAdmin().getServerAddress().split(",");
+        return new LoggingAppointAdminDiscovery(serverAddressArray);
+    }
+}
