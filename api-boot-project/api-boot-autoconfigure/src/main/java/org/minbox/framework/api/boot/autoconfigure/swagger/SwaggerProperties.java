6bd12557e0f5b1c099452850d02f95bf9587ca2a
hengboy <jnyuqy@gmail.com>
2019-12-17 09:40:31 +0800

    创建2.2.2-SNAPSHOT快照版本

--- api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
+++ api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
@@ -45,7 +45,7 @@ public class SwaggerProperties {
     /**
      * 文档版本号
      */
-    private String version = "2.2.1.RELEASE";
+    private String version = "2.2.2-SNAPSHOT";
     /**
      * 文档版权
      */
