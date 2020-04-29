a0122bcf36694007f1705104535e7ae4022801fb
hengboy <jnyuqy@gmail.com>
2020-04-29 10:54:04 +0800

    发布v2.2.4版本

--- api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
+++ api-boot-project/api-boot-autoconfigure/src/main/java/org/minbox/framework/api/boot/autoconfigure/swagger/SwaggerProperties.java
@@ -45,7 +45,7 @@ public class SwaggerProperties {
     /**
      * 文档版本号
      */
-    private String version = "2.2.3-SNAPSHOT";
+    private String version = "2.2.4.RELEASE";
     /**
      * 文档版权
      */
