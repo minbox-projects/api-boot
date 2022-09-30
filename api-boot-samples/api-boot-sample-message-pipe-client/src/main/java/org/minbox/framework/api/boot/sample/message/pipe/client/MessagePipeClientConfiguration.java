5f8393f4c6dfc3b1bbad1e465eebd2f3d7e9a7bd
恒宇少年 <39233436+hengboy@users.noreply.github.com>
2022-09-30 11:28:51 +0800

    :art: 调整message-pipe示例

--- api-boot-samples/api-boot-sample-message-pipe-client/src/main/java/org/minbox/framework/api/boot/sample/message/pipe/client/MessagePipeClientConfiguration.java
+++ api-boot-samples/api-boot-sample-message-pipe-client/src/main/java/org/minbox/framework/api/boot/sample/message/pipe/client/MessagePipeClientConfiguration.java
@@ -1,5 +1,6 @@
 package org.minbox.framework.api.boot.sample.message.pipe.client;
 
+import com.alibaba.cloud.nacos.NacosConfigProperties;
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.PropertyKeyConst;
 import com.alibaba.nacos.api.exception.NacosException;
@@ -8,6 +9,7 @@ import org.minbox.framework.message.pipe.spring.annotation.ServerServiceType;
 import org.minbox.framework.message.pipe.spring.annotation.client.EnableMessagePipeClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Properties;
 
@@ -21,18 +23,27 @@ import java.util.Properties;
 @Configuration
 @EnableMessagePipeClient(serverType = ServerServiceType.NACOS)
 public class MessagePipeClientConfiguration {
+    private NacosConfigProperties nacosConfigProperties;
+
+    public MessagePipeClientConfiguration(NacosConfigProperties nacosConfigProperties) {
+        this.nacosConfigProperties = nacosConfigProperties;
+    }
+
     /**
-     * 配置Nacos {@link NamingService}实例
+     * 配置{@link NamingService}服务实例
      *
-     * @return
+     * @return The {@link NamingService} instance
      * @throws NacosException
      */
     @Bean
     public NamingService namingService() throws NacosException {
         Properties properties = new Properties();
-        properties.put(PropertyKeyConst.USERNAME, "nacos");
-        properties.put(PropertyKeyConst.PASSWORD, "nacos");
-        properties.put(PropertyKeyConst.SERVER_ADDR, "open.nacos.yuqiyu.com:80");
+        properties.put(PropertyKeyConst.USERNAME, nacosConfigProperties.getUsername());
+        properties.put(PropertyKeyConst.PASSWORD, nacosConfigProperties.getPassword());
+        properties.put(PropertyKeyConst.SERVER_ADDR, nacosConfigProperties.getServerAddr());
+        if (!ObjectUtils.isEmpty(nacosConfigProperties.getNamespace())) {
+            properties.put(PropertyKeyConst.NAMESPACE, nacosConfigProperties.getNamespace());
+        }
         return NacosFactory.createNamingService(properties);
     }
 }
--- api-boot-samples/api-boot-sample-message-pipe-server/src/main/java/org/minbox/framework/api/boot/sample/message/pipe/server/MessagePipeServerConfiguration.java
+++ api-boot-samples/api-boot-sample-message-pipe-server/src/main/java/org/minbox/framework/api/boot/sample/message/pipe/server/MessagePipeServerConfiguration.java
@@ -1,16 +1,15 @@
 package org.minbox.framework.api.boot.sample.message.pipe.server;
 
+import com.alibaba.cloud.nacos.NacosConfigProperties;
 import com.alibaba.nacos.api.NacosFactory;
 import com.alibaba.nacos.api.PropertyKeyConst;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
-import org.minbox.framework.message.pipe.server.processing.push.PushMessageEvent;
 import org.minbox.framework.message.pipe.spring.annotation.ServerServiceType;
 import org.minbox.framework.message.pipe.spring.annotation.server.EnableMessagePipeServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Properties;
 
@@ -22,18 +21,27 @@ import java.util.Properties;
 @Configuration
 @EnableMessagePipeServer(serverType = ServerServiceType.NACOS)
 public class MessagePipeServerConfiguration {
+    private NacosConfigProperties nacosConfigProperties;
+
+    public MessagePipeServerConfiguration(NacosConfigProperties nacosConfigProperties) {
+        this.nacosConfigProperties = nacosConfigProperties;
+    }
+
     /**
-     * 配置Nacos {@link NamingService}实例
+     * 配置{@link NamingService}服务实例
      *
-     * @return
+     * @return The {@link NamingService} instance
      * @throws NacosException
      */
     @Bean
     public NamingService namingService() throws NacosException {
         Properties properties = new Properties();
-        properties.put(PropertyKeyConst.USERNAME, "nacos");
-        properties.put(PropertyKeyConst.PASSWORD, "nacos");
-        properties.put(PropertyKeyConst.SERVER_ADDR, "open.nacos.yuqiyu.com:80");
+        properties.put(PropertyKeyConst.USERNAME, nacosConfigProperties.getUsername());
+        properties.put(PropertyKeyConst.PASSWORD, nacosConfigProperties.getPassword());
+        properties.put(PropertyKeyConst.SERVER_ADDR, nacosConfigProperties.getServerAddr());
+        if (!ObjectUtils.isEmpty(nacosConfigProperties.getNamespace())) {
+            properties.put(PropertyKeyConst.NAMESPACE, nacosConfigProperties.getNamespace());
+        }
         return NacosFactory.createNamingService(properties);
     }
 }
